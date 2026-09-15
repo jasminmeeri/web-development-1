5283a2d90923c9868886c245447fc014da260551
Jasmin Järvinen <jasmin.jarvinen@gmail.com>
2026-09-15 22:17:44 +0300

    Changes made to scritp.js and index.html

--- WS02_JS_basics/js/script.js
+++ WS02_JS_basics/js/script.js
@@ -1,1 +1,42 @@
+// Step 1
 
+console.log("Hello World!"); 
+
+// Step 2
+
+const userName = "Jasmin";
+const favoriteAnimal = "hamster";
+
+console.log("My name is " + userName);
+console.log("and my favorite animal is " + favoriteAnimal + ".");
+
+// Step 3
+
+const visitorName = prompt("What is your name?");
+console.log("Hello, " + visitorName + "! Welcome to learn JavaScript with me!");
+
+// Step 4
+
+const visitorAge = prompt("How old are you?");
+if (visitorAge < 18) {
+    console.log("You are under 18.");
+} else {
+    console.log("You are an adult.");
+}
+
+// Step 5 
+
+function greetUser(name) {
+    console.log("Hello, " + name + "! It is nice to meet you.");
+}
+
+greetUser("Jasmin");
+greetUser("Nick");
+greetUser("Alex");
+
+// Step 6
+
+const button = document.getElementById("myButton");
+button.addEventListener("click", function() {
+    alert("Button clicked! Everything seems to be working!");
+});
